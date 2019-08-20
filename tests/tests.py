208c986c5f230d2d75c6303aaeb351b6b26a43d1
raphaelcjamarante <39167880+raphaelcjamarante@users.noreply.github.com>
2019-08-20 15:18:12 +0200

    a few more tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,9 +2,12 @@
 
 import unittest
 
-import app.process
-import app.labels
+import app.docs
 import app.filters
+import app.labels
+import app.process
+import app.utilities
+import app.model.batch
 
 class TestsUnit(unittest.TestCase):
 
@@ -12,28 +15,67 @@ class TestsUnit(unittest.TestCase):
         nbrcmds = 5
         nbrmedic = 30
         site_filter = "/filter/store_id/pharmashopi"
+
         livraison = ""
         cmds = app.filters.filtrage_picking(nbrcmds, nbrmedic, site_filter, livraison="")
-        assert len(cmds.keys()) == nbrcmds
+        assert len(cmds.keys()) <= nbrcmds
 
-        #TODO : assert all are one livraison
+        livraison = ["Colissimo", "Mondial Relay", "Lettre suivie"]
+        for item in livraison:
+            cmds = app.filters.filtrage_picking(nbrcmds, nbrmedic, site_filter, livraison=item)
+            assert len(cmds.keys()) <= nbrcmds
+            for key in cmds:
+                shipping_name = cmds[key]['shipping']['method_name']
+                assert item in shipping_name
+
+    #def test_filtrage_arnaque(self):
+
+    def test_picking(self):
+        mode_teste = True
+        sortie = 3
+        cmds = app.utilities.get_request(f"api/orders/filter/status/1/filter/orderby/date_created/asc/limit/5")
+        batch = app.model.batch.Batch(cmds, mode_teste)
+        
+        app.process.picking(batch, sortie, mode_teste)
+
+    def test_docs(self):
+        mode_teste = True
+        livraison = ''
+        cmds = app.utilities.get_request(f"api/orders/filter/status/1/filter/orderby/date_created/asc/limit/5")
+        batch = app.model.batch.Batch(cmds, mode_teste)
+
+        app.docs.picking_doc(batch, livraison)
+        app.docs.bonprep_doc(batch.cmds, livraison)
 
 class TestsIntegrated(unittest.TestCase):
 
     def test_picking(self):
-
+        nbrcmds = 5
+        nbrmedic = 30
+        sortie = 3
         sites = {"Pharmashopi": True, "Espace Contention": False}
-        app.process.bonetpick(5, 30, sites, 3, "", 3, True)
-        #app.process.bonetpick(5, 30, sites, 3, "Colissimo", 3, True)
-        #app.process.bonetpick(5, 30, sites, 3, "Mondial Relay", 3, True)
-        #app.process.bonetpick(5, 30, sites, 3, "Lettre suivie", 3, True)
-        app.process.bonetpick(0, 30, sites, 3, "", 3, True)
+        livraison = ['', 'Colissimo', 'Mondial Relay', 'Lettre suivie']
+        nbrjours = 3
+        mode_teste = True
+
+        for item in livraison:
+            app.process.bonetpick(nbrcmds, nbrmedic, sites, sortie, item, nbrjours, mode_teste)
+
+        nbrcmds = 0
+        app.process.bonetpick(nbrcmds, nbrmedic, sites, sortie, '', nbrjours, mode_teste)
         
 
     def test_detection(self):
-
+        nbrcmds = 10
         sites = {"Pharmashopi": True, "Espace Contention": False}
-        app.process.detection_arnaque(10, sites, 3, True)
+        nbrjours = 3
+        mode_teste = True
+        mode_detection = ['batch', 'single']
+        id_cmd = 357000
+
+        app.process.detection_arnaque(nbrcmds, sites, nbrjours, mode_teste, mode_detection[0])
+        app.process.detection_arnaque(nbrcmds, sites, nbrjours, mode_teste, mode_detection[1], id_cmd)
+
 
     def test_labels(self):
 
